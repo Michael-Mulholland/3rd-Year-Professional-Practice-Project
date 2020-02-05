a17c1850bd53f0f9ab51f40cea1ac0546bff2a11
Michael Mulholland <G00362383@gmit.ie>
2020-02-05 20:59:13 +0000

    1st commit - Added a floor and a player. Press the space bar to move the player in an upwards direction and release to make the player fall back to the floor using gravity

---
+++ Assets/__Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // reference to RigidBody
+    public Rigidbody rb; 
+
+    // speed in which our player will move upwards
+    public float upwardSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check to see if the user is pressing the Spacebar
+        if(Input.GetKey(KeyCode.Space)){
+            // once the Space key is pressed, apply force upwards onto gameObject (Player)
+            rb.AddForce(new Vector3(0, upwardSpeed));
+        }
+    }
+}
